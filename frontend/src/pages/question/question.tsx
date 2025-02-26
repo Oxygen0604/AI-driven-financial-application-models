e72045b5f90236abf9f72e8aeae4e46fc7762c4f
Chen Yin <923684815@qq.com>
2025-02-26 23:35:51 +0800

    完善home和question界面

--- frontend/src/pages/question/question.tsx
+++ frontend/src/pages/question/question.tsx
@@ -1,25 +1,43 @@
 import React from'react';
 import "./question.scss";
 import Shell from '../../component/shell/shell';
+import ChatBox from '../../component/chatBox/chatBox';
 
 const Question = () => {
+
+
   return (
     <div className="question">
       <div className='shell'>
         <Shell />
       </div>
 
-      <div className='header'>
+      <div className='container'>
+        <div className='header'>
 
+        </div>
+
+        <div className='content'>
+          <ChatBox type={false} value='11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111'/>
+          <ChatBox type={true} value='1111111111111111111111111111111111111111111111111111111'/>
+        </div>
+
+        <div className='footer'>
+          <div className='input'>
+            <textarea placeholder='请输入你的问题'/>
+          </div>
+
+          <div className='decoration'>
+            AI-DRIVEN FINANTIAL APPLICATION<br/>
+            ANALYSE HELPER
+          </div>
+
+          <div className='send'>
+            发送
+          </div>
+        </div>
       </div>
-
-      <div className='content'>
-
-      </div>
-
-      <div className='footer'>
-
-      </div>
+      
     </div>
   );
 }
